b0fe880750c9252b2641979eeedc3138e84cc057
Opilane TTHK <opilane@tthk.ee>
2020-11-11 11:28:32 +0200

    Add proect

---
+++ osa2_valjataga/osa2_valjataga/varian2.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace osa2_valjataga
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class varian2 : ContentPage
+    {
+        Label label;
+        DatePicker datePicker;
+        public varian2()
+        {
+            InitializeComponent();
+        }
+        /*
+        private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            label.Text = "Вы выбрали " + e.NewDate.ToString("dd/MM/yyyy");
+            if (e.NewDate.ToString("dd/MM/yyyy") == "20/03/2021")
+            {
+                label.Text = "Овен";
+
+                
+            }
+            if (e.NewDate.ToString("dd/MM/yyyy") == "21/04/2021")
+            {
+                label.Text = "Телец";
+            }
+            if (e.NewDate.ToString("dd/MM/yyyy") == "21/05/2021")
+            {
+                label.Text = "Близнецы";
+                
+            }
+            if (e.NewDate.ToString("dd/MM/yyyy") == "21/06/2021")
+            {
+                label.Text = "Рак";
+            }
+        }
+        */
+        /*
+        private void data_DateSelected_1(object sender, DateChangedEventArgs e)
+        {
+            label.Text = "Вы выбрали " + e.NewDate.ToString("dd/MM/yyyy");
+            if (e.NewDate.ToString("dd/MM/yyyy") == "20/03/2021")
+            {
+                label.Text = "Овен";
+                horos.Source = "oven.png";
+
+            }
+
+        }
+        */
+
+        private void changehoros_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            label.Text =  e.NewDate.ToLongDateString();
+            if (e.NewDate.ToString("dd/MM/yyyy") == "20/03/2021")
+            {
+                horos.Source = "oven.png";
+
+
+            }
+        }
+    }
+}
